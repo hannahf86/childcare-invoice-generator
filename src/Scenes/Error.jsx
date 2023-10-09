700160b36b8cc230f900e9cd175726795fab068f
hannahf86 <hannahfeehan.design@gmail.com>
2023-10-09 14:08:20 +0100

    Error page completed

--- src/Scenes/Error.jsx
+++ src/Scenes/Error.jsx
@@ -1,7 +1,28 @@
+import { Link, useNavigate, useRouteError } from "react-router-dom"
+
+// ASSETS
+import { HiHome, HiArrowSmLeft } from 'react-icons/hi'
+
 const Error = () => {
+
+    const error = useRouteError();
+    const navigate = useNavigate();
+
     return (
-        <div>
-            Error
+        <div className="error">
+            <h1>Uh oh! There's been a problem!</h1>
+            <p>{error.message || error.statusText}</p>
+            <div className="flex-md">
+                <button className="btn btn--dark" onClick={() => navigate(-1)}>
+                    <HiArrowSmLeft width={20} />
+                    <span>Go Back</span>
+                </button>
+
+                <Link to='/' className="btn btn--dark" >
+                    <HiHome width={20} />
+                    <span>Go home</span>
+                </Link>
+            </div>
         </div>
     )
 }
