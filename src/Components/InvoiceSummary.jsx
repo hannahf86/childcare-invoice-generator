ed519ee4a3a1b583b67891cda9a40665f78aa112
hannahf86 <hannahfeehan.design@gmail.com>
2023-10-21 14:49:43 +0100

    Conditionals altered

--- src/Components/InvoiceSummary.jsx
+++ src/Components/InvoiceSummary.jsx
@@ -3,24 +3,10 @@ import React from 'react'
 const InvoiceSummary = ({ child }) => {
     const { familyNameId, name, hoursPerWeek, funding } = child;
 
-    console.log(familyNameId)
-
     return (
         <div >
             {
-                familyNameId !== familyNameId ? (
-                    <div className='invoice-alt'>
-                        <div className="card-text">
-                            <p><strong>Name: </strong>{name}</p>
-                            <p><strong>Hours this week: </strong>{hoursPerWeek} </p>
-                        </div>
-
-                        <div>
-                            <p><strong>Funding applied: </strong><br />{funding}</p>
-                        </div>
-
-                    </div>
-                ) : (
+                familyNameId === familyNameId ? (
                     <div className='invoice'>
                         <div className="card-text">
                             <p><strong>Name: </strong>{name}</p>
@@ -30,13 +16,24 @@ const InvoiceSummary = ({ child }) => {
                         <div>
                             <p><strong>Funding applied: </strong><br />{funding}</p>
                         </div>
+                    </div>
+                ) : (
+                    <div className='invoice-alt'>
+                        <div className="card-text">
+                            <p><strong>Name: </strong>{name}</p>
+                            <p><strong>Hours this week: </strong>{hoursPerWeek} </p>
+                        </div>
 
+                        <div>
+                            <p><strong>Funding applied: </strong><br />{funding}</p>
+                        </div>
                     </div>
                 )
             }
-
         </div>
+
     )
 }
+
 
 export default InvoiceSummary
