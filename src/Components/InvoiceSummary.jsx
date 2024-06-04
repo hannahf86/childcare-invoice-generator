8ee5452906d328657017a35143db4b9efe44a92b
hannahf86 <hannahfeehan.design@gmail.com>
2024-06-04 11:24:07 +0100

    dashboard updated

--- src/Components/InvoiceSummary.jsx
+++ src/Components/InvoiceSummary.jsx
@@ -1,59 +1,69 @@
-import React from 'react'
-import { useEffect } from 'react';
-    // --------------------------------
-    // THE MATHS...
+import React from "react";
+import { useEffect } from "react";
+// --------------------------------
+// THE MATHS...
 
-    // const total = totalWeeklyHours * rate;
+// const total = totalWeeklyHours * rate;
 
-    useEffect(() => {
-        if (childsAge <= 2) {
-            setRate(5.5);
-        } else if (childsAge <= 4) {
-            setRate(5);
-        } else {
-            setRate(4.5);
-        }
-    }, [childsAge]);
+// useEffect(() => {
+//     if (childsAge <= 2) {
+//         setRate(5.5);
+//     } else if (childsAge <= 4) {
+//         setRate(5);
+//     } else {
+//         setRate(4.5);
+//     }
+// }, [childsAge]);
 
-    useEffect(() => {
-        if (funding === "2 year old, 15 hours funding") {
-            setFundingHours(15);
-        } else if (funding === "3 year old, 15 hours funding") {
-            setFundingHours(15);
-        } else if (funding === "3 year old, 30 hours funding") {
-            setFundingHours(30);
-        } else {
-            setFundingHours(0);
-        }
-    }, [funding])
+// useEffect(() => {
+//     if (funding === "2 year old, 15 hours funding") {
+//         setFundingHours(15);
+//     } else if (funding === "3 year old, 15 hours funding") {
+//         setFundingHours(15);
+//     } else if (funding === "3 year old, 30 hours funding") {
+//         setFundingHours(30);
+//     } else {
+//         setFundingHours(0);
+//     }
+// }, [funding])
 
-    const total = childsAge * rate;
+const childTotal = document.getElementById("AddChildForm.totalAmount");
 
-
-const InvoiceSummary = ({ childrenArray, familyName, }) => {
-
-
-    return (
-        <div className='invoice'>
-        <h3 className='card-family-name'><strong>Name: </strong>{familyName}</h3>
-        <ul className="card-text">
-            {childrenArray.map(child => (
-                <li key={child.id} className='list-item'>
-                    <p><strong>First Name: </strong>{child.name}</p>
-                    <p><strong>Age: </strong>{child.childsAge}</p>
-                    <p><strong>Hours per Week: </strong>{child.hoursPerWeek}</p>
-                    <p><strong>Funding: </strong>{child.funding}</p>
-                    <p><strong>Total:</strong>TOTAL GOES HERE!</p>
-                </li>
-            ))}
-        </ul>
-
-
-
+const InvoiceSummary = ({}) => {
+  return (
+    <div className="invoice">
+      <h3 className="card-family-name">
+        <strong>Name: </strong>
+        {familyName}
+      </h3>
+      <ul className="card-text">
+        {childrenArray.map((child) => (
+          <li key={child.id} className="list-item">
+            <p>
+              <strong>First Name: </strong>
+              {child.name}
+            </p>
+            <p>
+              <strong>Age: </strong>
+              {child.childsAge}
+            </p>
+            <p>
+              <strong>Hours per Week: </strong>
+              {child.hoursPerWeek}
+            </p>
+            <p>
+              <strong>Funding: </strong>
+              {child.funding}
+            </p>
+            <p>
+              <strong>Total:</strong>
+              {childTotal}
+            </p>
+          </li>
+        ))}
+      </ul>
     </div>
-    )
-}
+  );
+};
 
-
-
-export default InvoiceSummary
+export default InvoiceSummary;
--- src/Scenes/Dashboard.jsx
+++ src/Scenes/Dashboard.jsx
@@ -1,151 +1,143 @@
-import React, { useState, useRef } from 'react';
-
-// ICONS
-import { HiPrinter, HiDownload } from 'react-icons/hi'
+import { useState } from "react";
 
 // LIBRARIES
 // import { useReactToPrint } from 'react-to-print'
 
-
-// REACT-ROUTER-DOM 
+// REACT-ROUTER-DOM
 import { useLoaderData } from "react-router-dom";
 
 // SCENES
 import Intro from "./Intro";
 import AddFamilyForm from "../Components/AddFamilyForm";
 import AddChildForm from "../Components/AddChildForm";
-import InvoiceSummary from "../Components/InvoiceSummary";
+// import InvoiceSummary from "../Components/InvoiceSummary";
 
 // TOAST
 import { toast } from "react-toastify";
 
 //  HELPERS
-import { fetchData, familyName, addChild, waait, } from "../Utilities/Helpers";
+import { fetchData, familyName, addChild, waait } from "../Utilities/Helpers";
 import { useEffect } from "react";
 
 // LOADERS
 export function dashboardLoader() {
-    const userName = fetchData("userName");
-    const familyName = fetchData("familyName");
-    const addChild = fetchData("addChild");
-    return { userName, familyName, addChild }
+  const userName = fetchData("userName");
+  const familyName = fetchData("familyName");
+  const addChild = fetchData("addChild");
+  return { userName, familyName, addChild };
 }
 
 // ACTIONS
 
 // add button delay
 export async function dashboardAction({ request }) {
-    await waait()
+  await waait();
 
-    const data = await request.formData();
-    const { _action, ...values } = Object.fromEntries(data)
+  const data = await request.formData();
+  const { _action, ...values } = Object.fromEntries(data);
 
-    // new user submission
-    if (_action === "newUser") {
-        try {
-            localStorage.setItem("userName", JSON.stringify(values.userName))
-            return toast.success(`Welcome, ${values.userName}`)
-        } catch (e) {
-            throw new Error("There was a problem creating your account.")
-        }
+  // new user submission
+  if (_action === "newUser") {
+    try {
+      localStorage.setItem("userName", JSON.stringify(values.userName));
+      return toast.success(`Welcome, ${values.userName}`);
+    } catch (e) {
+      throw new Error("There was a problem creating your account.");
     }
+  }
 
-    // new family added
-    if (_action === "familyName") {
-        try {
-            familyName({
-                familyName: values.familyName,
-            })
-            return toast.success("Family name added")
-        } catch (e) {
-            throw new Error("There was a problem creating your invoice.")
-        }
+  // new family added
+  if (_action === "familyName") {
+    try {
+      familyName({
+        familyName: values.familyName,
+      });
+      return toast.success("Family name added");
+    } catch (e) {
+      throw new Error("There was a problem creating your invoice.");
     }
+  }
 
-    // add child
-    if (_action === "addChild") {
-        try {
-            addChild({
-                name: values.addChild,
-                childsAge: values.childsAge,
-                hoursPerWeek: values.hoursPerWeek,
-                funding: values.funding,
-                familyNameId: values.selectedFamily
-            })
-            return toast.success("New child added!")
-        } catch (e) {
-            throw new Error("There was a problem creating your invoice.")
-        }
+  // add child
+  if (_action === "addChild") {
+    try {
+      addChild({
+        name: values.addChild,
+        childsAge: values.childsAge,
+        hoursPerWeek: values.hoursPerWeek,
+        funding: values.funding,
+        familyNameId: values.selectedFamily,
+      });
+      return toast.success("New child added!");
+    } catch (e) {
+      throw new Error("There was a problem creating your invoice.");
     }
+  }
 }
 
-
 const Dashboard = () => {
-    const { userName, familyName, addChild } = useLoaderData()
-    const [familyData, setFamilyData] = useState([]);
+  const { userName, familyName, addChild } = useLoaderData();
+  const [familyData, setFamilyData] = useState([]);
 
-    // PRINTING FUNCTION
-    // const componentRef = useRef();
-    // const handlePrint = useReactToPrint({
-    //     content: () => componentRef.current,
-    //     documentTitle: { familyName },
-    //     onAfterPrint: () => alert("Printing successful!")
-    // })
+  // PRINTING FUNCTION
+  // const componentRef = useRef();
+  // const handlePrint = useReactToPrint({
+  //     content: () => componentRef.current,
+  //     documentTitle: { familyName },
+  //     onAfterPrint: () => alert("Printing successful!")
+  // })
 
-    // DOWNLOAD FUNCTION
+  // DOWNLOAD FUNCTION
 
+  // Takes the raw data and makes it structured
+  useEffect(() => {
+    familyName?.length > 0 &&
+      // Puts everything from familyName and only the children with the matching fmilyNameId
+      setFamilyData(
+        familyName.map((f) => ({
+          ...f,
+          childrenArray: addChild?.filter((c) => c.familyNameId === f.id) || [],
+        })),
+      );
+  }, [familyName, addChild]);
 
-    // Takes the raw data and makes it structured
-    useEffect(() => {
-        familyName?.length > 0 &&
+  return (
+    <>
+      {userName ? (
+        <div className="dashboard">
+          <h1>
+            Welcome back, <span className="accent">{userName}</span>
+          </h1>
 
-            // Puts everything from familyName and only the children with the matching fmilyNameId
-            setFamilyData(familyName.map(f => ({
-                ...f,
-                childrenArray: addChild?.filter(c => c.familyNameId === f.id) || []
-            })))
-    }, [familyName, addChild])
+          <div className="grid-sm">
+            {!familyData && <p>Add a family name below to get started.</p>}
+            <AddFamilyForm />
+          </div>
 
-    return (
-        <>
-            {
-                userName ? (
-                    <div className="dashboard">
-                        <h1>Welcome back, <span className="accent">{userName}</span></h1>
+          <div className="grid-sm">
+            {familyData?.map((f) => (
+              <div className="grid-lg" key={f.id}>
+                <div className="flex-lg">
+                  <AddChildForm familyName={familyName} />
+                </div>
 
-                        <div className="grid-sm">
-                            {!familyData && (
-                                <p>Add a family name below to get started.</p>
-                            )}
-                            <AddFamilyForm />
-                        </div>
-
-                        <div className="grid-sm">
-                            {familyData?.map(f => (
-                                <div className="grid-lg" key={f.id}>
-                                    <div className="flex-lg">
-                                        <AddChildForm familyName={familyName} />
-                                    </div>
-
-                                    <h2>Existing Invoices</h2>
-                                    <div className="invoices" >
-                                        {
+                <h2>Existing Invoices</h2>
+                <div className="invoices">
+                  {/* {
                                             <InvoiceSummary 
                                             familyName={f.familyName} 
                                             childrenArray={f.childrenArray} 
                                             />
-                                        }
-                                       
-                                    </div>
-                                </div>
-                            ))}
-
-
-                        </div>
-                    </div >
-                ) : <Intro />
-            }
-        </>
-    )
-}
-export default Dashboard
+                                        } */}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      ) : (
+        <Intro />
+      )}
+    </>
+  );
+};
+export default Dashboard;
